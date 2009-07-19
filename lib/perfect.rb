ea7a60218427322968f7cc77452771599f8bac69
Run Paint Run Run <runrun@runpaint.org>
2009-07-19 20:36:37 +0100

    Add #proper_divisors

--- lib/perfect.rb
+++ lib/perfect.rb
@@ -1,5 +1,10 @@
 class Integer
   def perfect?
+    return false unless self > 0
     self.to_s =~ /(6|8)$/ ? true : false
+  end
+
+  def proper_divisors
+    (1...self).select { |n| (self % n).zero? }
   end
 end
--- spec/perfect_spec.rb
+++ spec/perfect_spec.rb
@@ -8,4 +8,30 @@ describe Integer, "#perfect?" do
   it "returns false for 33" do
     33.perfect?.should be_false
   end
+
+  it "returns false for 8" do
+    8.perfect?.should be_false
+  end
+
+  it "returns false for negative integers" do
+    -6.perfect?.should be_false
+  end
+end
+
+describe Integer, "#proper_divisors" do
+  it "returns an Array" do
+    6.proper_divisors.should be_an_instance_of(Array)
+  end
+
+  it "returns integers that divide into self with no remainder" do
+    6.proper_divisors.should include(2, 3)
+  end
+
+  it "doesn't return integers that divide into self with a remainder" do
+    6.proper_divisors.should_not include(4, 5)
+  end
+
+  it "doesn't return itself as a divisor" do
+    9.proper_divisors.should_not include(9)
+  end
 end
