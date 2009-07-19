56891e9f2a4ba91c1b78bbb7d9b2976a5ba5a03c
Run Paint Run Run <runrun@runpaint.org>
2009-07-19 18:48:16 +0100

    Integer#perfect?: Should return true for 6.

--- spec/perfect_spec.rb
+++ spec/perfect_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Perfect" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe Integer, "#perfect?" do
+  it "returns true for 6" do
+    6.perfect?.should be_true
+   end
 end
