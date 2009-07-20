1ba6bd5ef47a4999e5d315da47360a8926710063
Run Paint Run Run <runrun@runpaint.org>
2009-07-20 18:33:35 +0100

    Refactor

--- lib/perfect.rb
+++ lib/perfect.rb
@@ -1,10 +1,20 @@
 class Integer
+
   def perfect?
-    return false unless self > 0
-    proper_divisors.inject(0) {|sum, div| sum + div} == self
+    return false if self < 6 or self.odd? or self.to_s !~ /(6|8)$/
+    return false if self != 6 and digital_root != 1  
+    divisors = proper_positive_divisors
+    return false unless (divisors.size + 1).even?
+    divisors.reduce(:+) == self
   end
 
-  def proper_divisors
-    (1...self).select { |n| (self % n).zero? }
+  def proper_positive_divisors
+    return [] unless self > 1
+    (2..Math.sqrt(self).floor).select { |n| (self % n).zero? }.
+                              map {|n| [n, self/n]}.flatten << 1
+  end
+
+  def digital_root
+    self == 0 ? 0 : 1 + ((self - 1) % 9)
   end
 end
--- spec/perfect_spec.rb
+++ spec/perfect_spec.rb
@@ -1,16 +1,41 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Integer, "#perfect?" do
-  it "returns true for 6" do
-    6.perfect?.should be_true
+  it "returns true for the first 7 perfect numbers" do
+    [6, 28, 496, 8128, 33550336, 8589869056, 137438691328].each do |n|
+      n.perfect?.should be_true
+    end
   end
 
-  it "returns false for 33" do
-    33.perfect?.should be_false
+  it "returns false for the neighbours of the first 7 perfect numbers" do
+    [6, 28, 496, 8128, 33550336, 8589869056, 137438691328].each do |n|
+      (n+1).perfect?.should be_false
+      (n-1).perfect?.should be_false
+    end
   end
 
-  it "returns false for 8" do
-    8.perfect?.should be_false
+  it "returns false for odd integers" do
+    [29,31,33,35].each do |n|
+      n.perfect?.should be_false
+    end
+  end
+
+  it "returns false for 1" do
+    1.perfect?.should be_false
+  end
+
+  it "returns false for 0" do
+    0.perfect?.should be_false
+  end
+
+  it "returns false for -1" do
+    -1.perfect?.should be_false
+  end
+
+  it "returns false for non-perfect positive integers" do
+    [8, 66, 3478].each do |n|
+      n.perfect?.should be_false
+    end
   end
 
   it "returns false for negative integers" do
@@ -18,20 +43,61 @@ describe Integer, "#perfect?" do
   end
 end
 
-describe Integer, "#proper_divisors" do
+describe Integer, "#proper_positive_divisors" do
   it "returns an Array" do
-    6.proper_divisors.should be_an_instance_of(Array)
+    6.proper_positive_divisors.should be_an_instance_of(Array)
   end
 
   it "returns integers that divide into self with no remainder" do
-    6.proper_divisors.should include(2, 3)
+    6.proper_positive_divisors.should include(2, 3)
   end
 
   it "doesn't return integers that divide into self with a remainder" do
-    6.proper_divisors.should_not include(4, 5)
+    6.proper_positive_divisors.should_not include(4, 5)
   end
 
   it "doesn't return itself as a divisor" do
-    9.proper_divisors.should_not include(9)
+    9.proper_positive_divisors.should_not include(9)
+  end
+
+  it "returns all of the proper divisors of self" do
+    33.proper_positive_divisors.sort.should == [1, 3, 11]
+  end
+
+  it "returns [] for 1" do
+    1.proper_positive_divisors.should == []
+  end
+
+  it "returns [] for 0" do
+    0.proper_positive_divisors.should == []
+  end
+
+  it "returns [] for -1" do
+    -1.proper_positive_divisors.should == []
+  end
+
+  it "returns [] when self is negative" do
+    -32.proper_positive_divisors.should == []
+  end
+end
+
+describe Integer, "#digital_root" do
+  it "returns each single-digit number as is" do
+    (0..9).each do |n|
+      n.digital_root.should == n
+    end
+  end
+
+  it "returns the sum of the digits of multi-digit numbers" do
+    18.digital_root.should == 9
+  end
+
+  it "returns a single digit even when the sum of the digits is greater than 9" do
+    65536.digital_root.should == 7
+  end
+
+  it "works for negative numbers" do
+    -4.digital_root.should == 5
+    -99.digital_root.should == 9
   end
 end
