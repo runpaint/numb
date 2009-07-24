9f802f3ff5b8d228a381116232bfa42210238af4
Run Paint Run Run <runrun@runpaint.org>
2009-07-24 22:54:38 +0100

    Add #frugal?

--- lib/perfect.rb
+++ lib/perfect.rb
@@ -42,6 +42,10 @@ class Integer
     digits.size < prime_division.flatten.reject{|d|d==1}.join.to_i.digits.size
   end
 
+  def frugal?
+    digits.size > prime_division.flatten.reject{|d|d==1}.join.to_i.digits.size
+  end
+
   def perfect?
     return false if self < 6 or self.odd? or self.to_s !~ /(6|8)$/
     return false if self != 6 and digital_root != 1  
