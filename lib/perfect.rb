a37523ed6fcfd18f8dc68ae4b3f5375f579e210a
Run Paint Run Run <runrun@runpaint.org>
2009-07-28 05:25:27 +0100

    Add #kaprekar?

--- lib/perfect.rb
+++ lib/perfect.rb
@@ -61,6 +61,18 @@ class Integer
     a.length == (b.length - 1)
   end
 
+  def kaprekar?
+    return true if self == 1
+    sdigits = (self ** 2).digits
+    (1..sdigits.size-1).each do |first|
+      a = sdigits.first(first).join.to_i
+      b = sdigits.last(sdigits.size-first).join.to_i
+      next if [a,b].any?{|n| n.zero?}
+      return true if (a + b) == self
+    end
+    false
+  end
+
   def carol?
     return true if self == 7
     return true if self == -1
