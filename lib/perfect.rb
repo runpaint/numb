ede6599208d6afe28aeeb5f66ed448f8a6e74a57
Run Paint Run Run <runrun@runpaint.org>
2009-07-29 05:47:50 +0100

    Add #undulating?

--- lib/perfect.rb
+++ lib/perfect.rb
@@ -270,6 +270,15 @@ class Integer
     end
   end
 
+  def undulating?
+    dig = digits
+    return false unless digits.size >= 3 and digits[0] != digits[1]
+    dig.each_with_index do |d,i| 
+      return false unless i.odd? ? d == dig[1] : d == dig[0]
+    end
+    true
+  end
+
   def proper_positive_divisors
     positive_divisors.reject {|d| d == self }
   end
