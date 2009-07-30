b64ed96460a599e2f54472a7e94dd70cacf3b464
Run Paint Run Run <runrun@runpaint.org>
2009-07-30 07:02:44 +0100

    Use #ω in #μ

--- lib/numb_the/möbius.rb
+++ lib/numb_the/möbius.rb
@@ -3,10 +3,10 @@ class Integer
 
   def μ
     case
-      when (self < 1)                                       then nil
-      when (square_free? and prime_factors.uniq.size.even?) then 1
-      when (square_free? and prime_factors.uniq.size.odd?)  then -1
-      when (not square_free?)                               then 0
+      when (self < 1)                 then nil
+      when (square_free? and ω.even?) then 1
+      when (square_free? and ω.odd?)  then -1
+      when (not square_free?)         then 0
     end
   end
 
