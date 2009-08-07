bf1083a0a4072548baeb213e23e23b7411fd721b
Run Paint Run Run <runrun@runpaint.org>
2009-08-07 20:35:24 +0100

    Rename project to 'numb'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'numb_the'
+require 'numb'
 
 Spec::Runner.configure do |config|
   
