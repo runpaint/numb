d6ceb8dfdb6707a7698053abced322192ce4df43
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 23:57:15 +0100

    Integer#frugal?: Document

--- lib/numb/frugal.rb
+++ lib/numb/frugal.rb
@@ -1,5 +1,15 @@
 # coding: utf-8
 class Integer
+
+  # A frugal number has more digits than the number of digits in its prime
+  # factorization (including exponents).
+  #
+  # Returns true if self is frugal; false otherwise.
+  #
+  #     128.frugal?  #=> true
+  #     256.frugal?  #=> true
+  #     300.frugal?  #=> false
+  #
   def frugal?
     digits.size > prime_division.flatten.reject{|d|d==1}.join.to_i.digits.size
   end
