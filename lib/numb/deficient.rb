db010bf29ce67546e3b60b735e627a455656c0a4
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 22:36:42 +0100

    Integer#deficient?: Document

--- lib/numb/deficient.rb
+++ lib/numb/deficient.rb
@@ -1,5 +1,15 @@
 # coding: utf-8
 class Integer
+  # A deficient number is a number n for which σ(n) < 2n. That is, the sum of
+  # its divisors are less than the number. (To calculate the sum of divisors
+  # for an arbitrary integer see Integer#σ).
+  #
+  # Returns true if the number is deficient; false otherwise.
+  #
+  #     8.deficient?  #=> true
+  #     27.deficient? #=> true
+  #     6.deficient?  #=> false
+  #
   def deficient?
     return false unless self > 0
     sum_of_divisors < (2 * self)
