9415ba821e66b169dad5c531548a362965e6bac3
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 22:57:54 +0100

    Integer#dudeney?: Document

--- lib/numb/dudeney.rb
+++ lib/numb/dudeney.rb
@@ -1,5 +1,14 @@
 # coding: utf-8
 class Integer
+  # A Dudeney number is a positive integer that is a perfect cube such that
+  # the sum of its decimal digits is the cube root of the number. 
+  #
+  # Returns true if self is a Dudeney number; false otherwise.
+  #
+  #     4913.dudeney?  #=> true
+  #     5832.dudeney?  #=> true
+  #     98.dudeney?    #=> false
+  #
   def dudeney?
     Math.cbrt(self) == self.digits.reduce(:+)
   end
