aef61f3c283298733e4497cd6ef33fec81ef76a6
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 22:32:00 +0100

    Integer#automorphic?: Document

--- lib/numb/automorphic.rb
+++ lib/numb/automorphic.rb
@@ -1,5 +1,22 @@
 # coding: utf-8
 class Integer
+
+  # An automorphic number is a number whose square terminates in the number
+  # itself. That is, k is automorphic if the final digits of k<sup>2</sup> are
+  # the digits of k.
+  #
+  # More generally, an <i>n</i>-automorphic number is one of the form
+  # <i>nk<sup>2</sup></i> which has its last digits equal to <i>k</i>.
+  # <i>n</i> may be supplied as an argument to this method; otherwise it
+  # defaults to 1.
+  #
+  # Returns true if the number is automorphic; false otherwise. Aliased to
+  # Integer#curious?.
+  #
+  #     25.automorphic?    #=> true
+  #     9376.automorphic?  #=> true
+  #     600.automorphic?   #=> true
+  #
   def automorphic?(n=1)
     (n * self ** 2).to_s.end_with? self.to_s
   end
