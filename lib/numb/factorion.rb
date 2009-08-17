5ba95cd55a6759964e8c6f79f371e6548c2ce11c
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 23:25:42 +0100

    Integer#factorion?: Document

--- lib/numb/factorion.rb
+++ lib/numb/factorion.rb
@@ -1,5 +1,14 @@
 # coding: utf-8
 class Integer
+  # A factorion is a number equal to the sum of the factorials of its decimal
+  # digits.
+  #
+  # Returns true if self is a factorion; false otherwise.
+  #
+  #     145.factorion?    #=> true
+  #     40585.factorion?  #=> true
+  #     200.factorion?    #=> false
+  #
   def factorion?
     [1, 2, 145, 40585].include? self
   end
