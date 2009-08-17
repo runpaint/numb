7fa3369f8e0d1d5e7b6cdfc37f7eff546dee7cdf
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 21:56:46 +0100

    Integer#carol?: Document

--- lib/numb/carol.rb
+++ lib/numb/carol.rb
@@ -1,5 +1,16 @@
 # coding: utf-8
+
 class Integer
+
+# A Carol number is an integer of the form <i>4<sup>n</sup> − 2<sup>n +
+# 1</sup> − 1</i>.
+#
+# This method returns true if self is a Carol number; false otherwise.
+#
+#     −1.carol?  #=> true 
+#     959.carol? #=> true 
+#     32.carol?  #=> false
+#
   def carol?
     return true if self == 7
     return true if self == -1
