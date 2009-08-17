04eaba3ccf21f45af38379db7ed1968da72616e4
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 23:02:15 +0100

    Integer#economical?: Document

--- lib/numb/economical.rb
+++ lib/numb/economical.rb
@@ -1,5 +1,15 @@
 # coding: utf-8
 class Integer
+  # A number which is either frugal or equidigital.
+  #
+  # Returns true if self is economical; false otherwise.
+  #
+  # See also Integer#equidigital? and Integer#frugal?.
+  #
+  #     243.economical?  #=> true
+  #     7.economical?    #=> true
+  #     989.economical?  #=> false
+  #
   def economical?
     equidigital? or frugal?
   end
