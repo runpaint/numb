6c166a1141fb8e2af3c7e087a793a701644786f2
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 23:29:07 +0100

    Integer#extravagant?: Alias to #wasteful?

--- lib/numb/extravagant.rb
+++ lib/numb/extravagant.rb
@@ -3,7 +3,8 @@ class Integer
   # An extravagant number has fewer digits than the number of digits in its
   # prime factorization (including exponents).
   #
-  # Returns true if self is extravagant; false otherwise.
+  # Returns true if self is extravagant; false otherwise. Aliased to
+  # Integer#wasteful?.
   #
   #     234.extravagant?  #=> true
   #     87.extravagant?   #=> true
@@ -12,4 +13,6 @@ class Integer
   def extravagant?
     digits.size < prime_division.flatten.reject{|d|d==1}.join.to_i.digits.size
   end
+
+  alias :wasteful? :extravagant?
 end
