710ee7293a6ccd489fd83f34e7716080140e265e
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 23:10:49 +0100

    Integer#emrip?: Document

--- lib/numb/emrip.rb
+++ lib/numb/emrip.rb
@@ -1,5 +1,14 @@
 # coding: utf-8
 class Integer
+  # An emrip is a prime whose reversed digits give a different prime. For
+  # example, 17 is an emrip because 71 is also prime.
+  #
+  # Returns true if self is an emrip; false otherwise.
+  #
+  #     1009.emrip?  #=> true
+  #     1193.emrip?  #=> true
+  #     7.emrip?     #=> false
+  #
   def emrip?
     return false unless prime?
     reversed = to_s.reverse.to_i
