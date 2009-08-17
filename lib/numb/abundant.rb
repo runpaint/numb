c2b60f75d581d0f74ac7f26ebd4dbd241a112a19
Run Paint Run Run <runrun@runpaint.org>
2009-08-17 22:08:44 +0100

    Integer#abundant?: Document

--- lib/numb/abundant.rb
+++ lib/numb/abundant.rb
@@ -1,5 +1,15 @@
 # coding: utf-8
 class Integer
+  # An abundant number is a number n for which σ(n) > 2n. That is, the sum of
+  # its divisors exceeds 2n. (See Integer#σ to compute the sum of the divisors
+  # of an arbitrary integer).
+  #
+  # Returns true if the number is abundant; false otherwise.
+  #
+  #     96.abundant?   #=> true
+  #     100.abundant?  #=> true
+  #     345.abundant?  #=> false
+  #
   def abundant?
     return false unless self > 0
     sum_of_divisors > (2 * self)
