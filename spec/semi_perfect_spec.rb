03bfa84bf2ab4266c556aa718b5ade33e9c2ef78
Run Paint Run Run <runrun@runpaint.org>
2010-01-03 22:15:53 +0000

    Integer#semi_perfect?: Specify Guy's 2^m p constraint

--- spec/semi_perfect_spec.rb
+++ spec/semi_perfect_spec.rb
@@ -17,6 +17,18 @@ describe Integer, "#semi_perfect?" do
     end
   end
 
+  # Guy, R. K. 
+  # "Almost Perfect, Quasi-Perfect, Pseudoperfect, Harmonic, Weird, Multiperfect and Hyperperfect Numbers." 
+  # §B2 in Unsolved Problems in Number Theory, 2nd ed. New York: Springer-Verlag, pp. 45-53, 1994
+  it "returns true for 2**m * p (where m >= 1 && p.prime? && (p < 2**(m+1) && p > 2**m)" do
+    [[4, 17], [4, 19], [4, 23], [4, 29], [4, 31], 
+     [5, 37], [5, 41], [5, 43], [5, 47], [5, 53], [5, 59], [5, 61],
+     [6, 67], [6, 71], [6, 73], [6, 79], [6, 83], [6, 89], [6, 97],
+     [6, 101],[6, 103],[6, 107],[6, 109],[6, 113],[6, 127]].shuffle.first(10).each do |m,p|
+      (2**m * p).should be_semi_perfect
+    end
+  end
+
   it "returns false if the number is not semi-perfect" do
     ((1..263).to_a - SEMI_PERFECT).each do |number|
       number.should_not be_semi_perfect
