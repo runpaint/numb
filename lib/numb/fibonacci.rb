fbb5fcd2d64499961550890c990a0ea9fb75a1c6
Run Paint Run Run <runrun@runpaint.org>
2010-01-05 23:48:44 +0000

    Integer#fibonacci?: Add

---
+++ lib/numb/fibonacci.rb
@@ -0,0 +1,8 @@
+class Integer
+  def fibonacci?
+    return true if self == 1
+    # Posamentier, Alfred; Lehmann, Ingmar (2007). The (Fabulous) FIBONACCI
+    # Numbers. Prometheus Books. pp. 305
+    [4, -4].map{|x| (5 * (self**2)) + x}.one? &:square?
+  end
+end
