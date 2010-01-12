5db851239eed143b8802120f0f4ec2a5c53989a3
Run Paint Run Run <runrun@runpaint.org>
2010-01-12 11:01:59 +0000

    Integer#sphenic?: Write in terms of #prime_signature

--- lib/numb/sphenic.rb
+++ lib/numb/sphenic.rb
@@ -1,8 +1,6 @@
 # coding: utf-8
 class Integer
   def sphenic?
-    (prime_factors.size == 3) &&
-    (prime_factors.uniq.size == 3) && 
-    (prime_factors.reduce(:*) == self)
+    prime_signature == [1, 1, 1]
   end
 end
