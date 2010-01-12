4f3eebc7af62fd6343ae908154a66652a3874dab
Run Paint Run Run <runrun@runpaint.org>
2010-01-12 05:30:33 +0000

    Integer#totient: Greatly improve algo with Euler product

--- lib/numb/totient.rb
+++ lib/numb/totient.rb
@@ -3,7 +3,8 @@ class Integer
   def φ
     return 1 if self == 1
     return self - 1 if prime?
-    (1..self).select{|n| n.coprime?(self)}.size
+    (prime_factors.uniq.map{|f| 1 - Rational(1, f)}.reduce(:*) * self).to_i
+    #(1..self).select{|n| n.coprime?(self)}.size
   end
 
   alias :totient :φ
