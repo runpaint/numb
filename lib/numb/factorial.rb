bbd3cdd138819218f357ebf3c23d766dbb0c612d
Run Paint Run Run <runrun@runpaint.org>
2010-01-22 17:20:05 +0000

    Integer#factorial?: Add

--- lib/numb/factorial.rb
+++ lib/numb/factorial.rb
@@ -3,4 +3,15 @@ class Integer
     return 1 if zero?
     (1..self).reduce(:*)
   end
+
+  def factorial?
+    divisors = self.divisors.sort
+    divisors.each_with_index do |d, i|
+      if divisors[i.succ] == d.succ
+        return true if d.factorial == self
+      else
+        return d.factorial == self
+      end
+    end
+  end
 end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -12,3 +12,24 @@ describe Integer, "#factorial" do
     end
   end
 end
+
+describe Integer, "#factorial?" do
+  # A000142
+  @seq = [1,1,2,6,24,120,720,5040,40320,362880,3628800,
+          39916800,479001600,6227020800,87178291200,
+          1307674368000,20922789888000,355687428096000,
+          6402373705728000,121645100408832000,
+          2432902008176640000]
+
+  @seq.first(10).each do |n|
+    it "should return true for factorial #{n}" do
+      n.should be_factorial
+    end
+  end
+
+  @seq.first(10).to_seq.invert.sample(10).each do |n|
+    it "should return false for non-factorial #{n}" do
+      n.should_not be_factorial
+    end
+  end
+end
