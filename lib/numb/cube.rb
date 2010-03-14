2b754ec8ed6f505ae295347d6ab988794a22f004
Run Paint Run Run <runrun@runpaint.org>
2010-03-14 04:28:49 +0000

    Integer#cube?: Simplify now we can ignore 1.8

--- lib/numb/cube.rb
+++ lib/numb/cube.rb
@@ -1,8 +1,5 @@
 class Integer
   def cube?
-    y = self < 0 ? (-self) : self
-    x = Math.cbrt(y)
-    # The hack below is seemingly needed for 1.8 compatibility
-    x.ceil.to_f.to_s == x.to_s
+    Math.cbrt(self).integer?
   end
 end
