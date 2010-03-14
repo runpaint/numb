221007dfcf346ffdc6a4752d8186f68a365cee37
Run Paint Run Run <runrun@runpaint.org>
2010-03-14 23:57:33 +0000

    Integer#full_reptend_prime?: Use #primitive_root?

--- lib/numb/full_reptend_prime.rb
+++ lib/numb/full_reptend_prime.rb
@@ -1,8 +1,5 @@
 class Integer
   def full_reptend_prime?
-    return false unless prime? and (10**(self-1)).modulo(self) == 1
-    (self - 2).downto(1).none? do |k|
-      (10**k).modulo(self) == 1
-    end
+    prime? and primitive_root?(10)
   end
 end
