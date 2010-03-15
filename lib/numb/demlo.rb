e901c147083cb1c92183ba3094727fc5233fcea0
Run Paint Run Run <runrun@runpaint.org>
2010-03-15 19:15:58 +0000

    Integer#demlo?: Add

--- lib/numb/demlo.rb
+++ lib/numb/demlo.rb
@@ -2,4 +2,8 @@ class Integer
   def demlo
     repunit ** 2
   end
+
+  def demlo?
+    square? and isqrt.repunit?(10)
+  end
 end
--- spec/numb/demlo_spec.rb
+++ spec/numb/demlo_spec.rb
@@ -1,14 +1,28 @@
-describe Integer, "#demlo" do
-  # A002477 
-  @seq = [1,121,12321,1234321,123454321,12345654321,
-          1234567654321,123456787654321,12345678987654321,
-          1234567900987654321,123456790120987654321,
-          12345679012320987654321,
-          1234567901234320987654321]
+# A002477 
+DEMLO = [1,121,12321,1234321,123454321,12345654321,
+        1234567654321,123456787654321,12345678987654321,
+        1234567900987654321,123456790120987654321,
+        12345679012320987654321,
+        1234567901234320987654321]
 
-  @seq.to_enum.with_index(1).each do |demlo, n|
+describe Integer, "#demlo" do
+  DEMLO.to_enum.with_index(1).each do |demlo, n|
     it "returns #{demlo} for #{n}" do
       n.demlo.should == demlo
+    end
+  end
+end
+
+describe Integer, "#demlo?" do
+  DEMLO.each do |n|
+    it "returns true for Demlo number #{n}" do
+      n.should be_demlo
+    end
+  end
+
+  DEMLO.to_seq.invert.sample(500).each do |n|
+    it "returns false for non-Demlo number #{n}" do
+      n.should_not be_demlo
     end
   end
 end
