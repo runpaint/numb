cdfcd9b2f69f961c38ee45d9b23dd0f834522731
Run Paint Run Run <runrun@runpaint.org>
2010-03-19 21:51:44 +0000

    Integer#infinitary_divisors: Add

---
+++ lib/numb/infinitary_divisors.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+class Integer
+  def infinitary_divisors
+    pf = Hash[prime_factors.uniq.map{|f| [f, 0]}]
+    bin = divisors.map do |d|
+      [d, pf.merge(Hash[d.prime_division]).
+        values.
+        map{|v| sprintf("%.#{to_s(2).size}b", v)}.join]
+    end
+    bin = Hash[bin]
+    target = bin[self].chars.map.with_index.to_a.select{|a,b| a == '0'}.map(&:last)
+    bin.select{|d,b| target.all?{|i| b[i] == '0'}}.keys.sort
+  end
+end
