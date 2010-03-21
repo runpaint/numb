93f00012bec7f4b5de7f21133f7505cd903b53e0
Run Paint Run Run <runrun@runpaint.org>
2010-03-21 23:33:13 +0000

    Integer#lucas2: Add

---
+++ lib/numb/lucas2.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+class Integer
+  def lucas2(p, q)
+    d = (p**2) - (4*q)
+    raise ArgumentError unless d > 0
+    d_root = d.sqrt
+    (Rational(p + d_root, 2)**self + Rational(p - d_root, 2)**self).round
+  end
+end
