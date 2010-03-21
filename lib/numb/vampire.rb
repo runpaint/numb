489a347aae9bc09a7caf68cf67d712457e9553b1
Run Paint Run Run <runrun@runpaint.org>
2010-03-21 06:06:55 +0000

    Integer#vampire?: Optimise

--- lib/numb/vampire.rb
+++ lib/numb/vampire.rb
@@ -1,11 +1,12 @@
 # coding: utf-8
 class Integer
   def vampire?
-    return false unless !prime? and digits.size.even?
-    digits.permutation.map do |perm|
-      [:first,:last].map {|pos| perm.send(pos,(digits.size/2)).join.to_i }.sort
-    end.uniq.any? do |fangs|
-      fangs.reduce(:*) == self
+    return false unless composite? and digits.size.even?
+    seen = {}
+    digits.permutation.any? do |perm|
+      fangs = [:first,:last].map {|pos| perm.send(pos,(digits.size/2)).join.to_i }.sort
+      next if seen.key?(fangs)
+      seen[fangs] = fangs.reduce(:*) == self
     end
   end
 end
--- spec/numb/vampire_spec.rb
+++ spec/numb/vampire_spec.rb
@@ -1,19 +1,19 @@
 describe Integer, "#vampire?" do
-  # http://www.research.att.com/~njas/sequences/A014575
-  VAMPIRE = [1260,1395,1435,1530,1827,2187,6880,102510,104260,
-             105210,105264,105750,108135,110758,115672,116725,
-             117067,118440,120600,123354,124483,125248,125433,
-             125460,125500,126027,126846,129640]
+  #A014575
+  @seq = [1260,1395,1435,1530,1827,2187,6880,102510,104260,
+          105210,105264,105750,108135,110758,115672,116725,
+          117067,118440,120600,123354,124483,125248,125433,
+          125460,125500,126027,126846,129640]
   
-  it "returns true for vampire numbers" do
-    VAMPIRE.each do |number|
-      number.should be_vampire 
+  @seq.each do |n|
+    it "returns true for vampire number #{n}" do
+      n.should be_vampire 
     end
   end
   
-  it "returns false for non-vampire numbers" do
-    [1259,22,1396].each do |number|
-      number.should_not be_vampire 
+  @seq.to_seq.invert.sample(10).each do |n|
+    it "returns false for non-vampire number #{n}" do
+      n.should_not be_vampire 
     end
   end
 end
