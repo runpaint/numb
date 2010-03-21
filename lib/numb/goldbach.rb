8273bab5c19d76bd51f774c33d242449f6b4e70b
Run Paint Run Run <runrun@runpaint.org>
2010-03-21 05:39:13 +0000

    Integer#goldbach?: Optimise

--- lib/numb/goldbach.rb
+++ lib/numb/goldbach.rb
@@ -1,6 +1,11 @@
 class Integer
   def goldbach?
     return false unless even? and self > 2
-    downto(2).any?{|n| n.prime? and (self - n).prime?}
+    #downto(2).any?{|n| n.prime? and (self - n).prime?}
+    Prime.each do |prime|
+      next if prime == 2
+      return true if (self - prime).prime?
+      return false if prime >= self
+    end
   end
 end
