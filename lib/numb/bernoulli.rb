eb62b63fa9202d721dd15d0c7a732f48ecc2d3af
Run Paint Run Run <runrun@runpaint.org>
2010-03-22 01:36:16 +0000

    Integer#bernoulli: Refactor to use recursive formula

--- lib/numb/bernoulli.rb
+++ lib/numb/bernoulli.rb
@@ -2,13 +2,12 @@
 class Integer
   # TODO: Consider http://cims.nyu.edu/~harvey/bernmm/
   def bernoulli
-    return -0.5.to_r if self == 1
-    return 0.to_r if odd?
-    a = []
-    0.upto(self) do |m|
-      a[m] = Rational(1, m + 1)
-      m.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
-    end
-    a.first
+    return 1 if zero?
+    m = self
+    (m.zero? ? 1 : 0) - (0...m).map do |k| 
+      m.choose(k) * Rational(k.bernoulli, m - k + 1)
+    end.reduce(:+)
   end
+
+  memoize :bernoulli
 end
