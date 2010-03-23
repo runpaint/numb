9d7815eee9df40af63d855142521d2433d7301ae
Run Paint Run Run <runrun@runpaint.org>
2010-03-23 08:03:40 +0000

    Integer#minimal?: Move to divisors.rb

--- lib/numb/divisors.rb
+++ lib/numb/divisors.rb
@@ -312,4 +312,11 @@ class Integer
 
   alias :number_of_divisors :τ 
   alias :d :τ 
+
+  def minimal?
+    div_n = number_of_divisors
+    (self-1).downto(1).all? do |n|
+      n.number_of_divisors != div_n
+    end
+  end
 end
--- lib/numb/minimal.rb
+++
@@ -1,8 +0,0 @@
-class Integer
-  def minimal?
-    div_n = number_of_divisors
-    (self-1).downto(1).all? do |n|
-      n.number_of_divisors != div_n
-    end
-  end
-end
