2c927f42b27ca864a156ff68e4771da49c5dfef4
Run Paint Run Run <runrun@runpaint.org>
2010-03-26 12:55:31 +0000

    Integer#factorial_of?: Add

--- lib/numb/factorial.rb
+++ lib/numb/factorial.rb
@@ -14,4 +14,16 @@ class Integer
       end
     end
   end
+
+  def factorial_of?
+    return false unless factorial?
+    return 1 if self == 1
+    pfacts = primaries
+    divisors.sort.take_while.with_index{|d,i| d == i.succ}.reverse_each do |d|
+      pfacts.all? do |b, e|
+        (1..Math.log(d,b)).map{|k| Rational(d, b**k).floor}.reduce(:+) == e
+      end and return d
+    end
+    nil
+  end
 end
--- spec/numb/factorial_spec.rb
+++ spec/numb/factorial_spec.rb
@@ -1,12 +1,12 @@
-describe Integer, "#factorial" do
-  # A000142
-  @seq = [1,1,2,6,24,120,720,5040,40320,362880,3628800,
-          39916800,479001600,6227020800,87178291200,
-          1307674368000,20922789888000,355687428096000,
-          6402373705728000,121645100408832000,
-          2432902008176640000]
+# A000142
+FACTORIAL = [1,1,2,6,24,120,720,5040,40320,362880,3628800,
+             39916800,479001600,6227020800,87178291200,
+             1307674368000,20922789888000,355687428096000,
+             6402373705728000,121645100408832000,
+             2432902008176640000]
 
-  @seq.each_with_index do |factorial, n|
+describe Integer, "#factorial" do
+  FACTORIAL.each_with_index do |factorial, n|
     it "should return #{factorial} for #{n}!" do
       n.factorial.should == factorial
     end
@@ -14,22 +14,30 @@ describe Integer, "#factorial" do
 end
 
 describe Integer, "#factorial?" do
-  # A000142
-  @seq = [1,1,2,6,24,120,720,5040,40320,362880,3628800,
-          39916800,479001600,6227020800,87178291200,
-          1307674368000,20922789888000,355687428096000,
-          6402373705728000,121645100408832000,
-          2432902008176640000]
-
-  @seq.first(10).each do |n|
+  FACTORIAL.first(10).each do |n|
     it "should return true for factorial #{n}" do
       n.should be_factorial
     end
   end
 
-  @seq.first(10).to_seq.invert.sample(10).each do |n|
+  FACTORIAL.first(10).to_seq.invert.sample(10).each do |n|
     it "should return false for non-factorial #{n}" do
       n.should_not be_factorial
+    end
+  end
+end
+
+describe Integer, "#factorial_of?" do
+  FACTORIAL.first(12).each_with_index do |factorial, n|
+    next if n.zero?
+    it "should return #{n} for #{factorial}" do
+      factorial.factorial_of?.should == n
+    end
+  end
+
+  FACTORIAL.first(10).to_seq.invert.sample(10).each do |n|
+    it "should return false for non-factorial #{n}" do
+      n.factorial_of?.should be_false
     end
   end
 end
